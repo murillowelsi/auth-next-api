e64cb08acbc4bf54eb915ec26cb4d00d3fbee7a0
Murillo Welsi <murillo.welsi@gmail.com>
2021-02-05 17:20:33 +0000

    asd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,21 +9,10 @@ const options = {
       maxAge: 30 * 24 * 60 * 60 // 30 days
     },
     providers: [
-      Providers.Email({
-        server: {
-          port: 465,
-          host: 'smtp.gmail.com',
-          secure: true,
-          auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-          },
-          tls: {
-            rejectUnauthorized: false,
-          },
-        },
-        from: process.env.EMAIL_FROM,
-      })
+      Providers.GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      }),
     ],
     database: process.env.DATABASE_URL
     
